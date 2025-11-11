80ee4a5add2017b3a5fbfa697b2ce821451c928f
Dibash <desktop555555874@gmail.com>
2025-11-11 19:46:29 +0545

    Refactor PHP to display name and age in HTML
    Refactored PHP code to use variables in HTML output.

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <?php
+$name = "Dibash";
+$age = 20;
+?>
 
-$name ="Dibash";
-$age =20;
-echo "My name is '$name' age is '$age'";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Intro Example of Variable</title>
+</head>
+<body>
+  <div class="card">
+    <h1 id="intro">
+      <?php
+        echo "My name is $name and my age is $age.";
+      ?>
+    </h1>
+  </div>
+</body>
+</html>
